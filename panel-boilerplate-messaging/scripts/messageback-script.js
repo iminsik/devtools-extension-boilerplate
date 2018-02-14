9f9f016f8aca54e241239c4609565f9e08305604
iminsik <iminsik@gmail.com>
2018-02-14 14:25:44 -0800

    added counter

--- panel-boilerplate-messaging/scripts/messageback-script.js
+++ panel-boilerplate-messaging/scripts/messageback-script.js
@@ -1,8 +1,10 @@
 // document.querySelector('button').addEventListener('click', function() {
 //  chrome.extension.sendMessage({action: 'message', content:"Changed by page"}, function(message){});
 // });
+var count = 0;
 document.querySelector('button').addEventListener('click', function() {
-    sendObjectToDevTools({content: "Changed by page"});
+    sendObjectToDevTools({content: "Changed by page: " + count});
+    count++;
 });
 function sendObjectToDevTools(message) {
     // The callback here can be used to execute something on receipt
